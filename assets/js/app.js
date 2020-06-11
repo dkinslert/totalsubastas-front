be90f1db6656608ee597ef03cdb1731bd7442d0d
Kinslert Daniel Virguez <dkinslert@gmail.com>
2020-06-11 05:02:10 -0400

    first commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,42 +1,43 @@
-$( document ).ready(function () {
-    $(".moreBox").slice(0, 3).show();
-    if ($(".blogBox:hidden").length != 0) {
-        $("#loadMore").show();
-    }		
-    $("#loadMore").on('click', function (e) {
-        e.preventDefault();
-        $(".moreBox:hidden").slice(0, 6).slideDown();
-        if ($(".moreBox:hidden").length == 0) {
-            $("#loadMore").fadeOut('slow');
-        }
-    });
-    $('#showpassword').on('click', function(e){
-        var x = document.getElementById("myInput");
-        e.preventDefault();
-        if (x.type === "password") {
-            x.type = "text";
-          } else {
-            x.type = "password";
-        }
-    })
-    $('#loadMore').on('click', function(e){
-      
-    })
-    $('.carousel-indicators li').click( function(){
-        $('.carousel-indicators li').removeClass('active');
-        $('.carousel-indicators li').removeClass('spinner-border spinner-border-sm');
-            var $this = $(this);
-            if (!$this.hasClass('active')) {
-                
-                $(this).addClass('spinner-border spinner-border-sm');
-            }
-           
-    })
-    
+$(document).ready(function () {
+  $(".moreBox").slice(0, 3).show();
+  if ($(".blogBox:hidden").length != 0) {
+    $("#loadMore ").show();
+  }
+  $("#loadMore").on("click", function (e) {
+    e.preventDefault();
+    console.log("Hello, World!");
+    $(".moreBox:hidden").slice(0, 6).slideDown();
+    if ($(".moreBox:hidden").length == 0) {
+      $("#loadMore ").fadeOut("slow");
+    }
+  });
+  $("#showpassword").on("click", function (e) {
+    var x = document.getElementById("myInput");
+    e.preventDefault();
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  });
+
+  $(".carousel-indicators li").click(function () {
+    $(".carousel-indicators li").removeClass("active");
+    $(".carousel-indicators li").removeClass(
+      "spinner-border spinner-border-sm"
+    );
+    var $this = $(this);
+    if (!$this.hasClass("active")) {
+      $(this).addClass("spinner-border spinner-border-sm");
+    }
+  });
+
+  $(".card-header").on("click", function () {
+    $("#angle").removeClass("fa-angle-down").addClass("fa-angle-up");
+  });
+  $(".card-header").on(".collapse", function () {
+    $(".fas").removeClass("fa-arrow-up").addClass("fa-arrow-down");
+  });
 });
 
-
-
-(function(){
-   
-}())
+(function () {})();
