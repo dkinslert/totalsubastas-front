c0528b4efe219cd2c205e316b401551da2b0921f
Daniel Virguez <dkinslert@gmail.com>
2020-06-22 13:03:21 -0430

    first commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -39,19 +39,42 @@ $(document).ready(function () {
   });
   $(".thumbs_auction-img ").click(function () {
       $(".thumbs_auction-img").removeClass("thumb-active");
-      $(".thumbs_auction-img").removeClass("thumb-active");
       var $this = $(this);
       if (!$this.hasClass("thumb-active")) {
         $(this).addClass("thumb-active");
       }
+  });
+    $(".carousel-ctrl-next").click(function () {
+      var $this = $(this);
+      
   });
   $('.caret-play_auction-gallery').on('click', function () {
     $('#homeCarousel').carousel('cycle');
   })
 
   $('.pills-tab').scrollspy({ target: '#pills-tab' })
- 
+  
+  $('#hammer').click(function(){
+     $('.fa-gavel').removeClass('fa-rotate-270')
+     $('.fa-gavel').addClass('fa-rotate-180')
+  })
+/* when a user clicks, toggle the 'is-animating' class */
+  $(".heart").on('click touchstart', function(){
+    $(this).toggleClass('is_animating');
+  });
 
+/*when the animation is over, remove the class*/
+  $(".heart").on('animationend', function(){
+    $(this).toggleClass('is_animating');
+  });
+  var bootCarousel = $(".carousel");
+  bootCarousel.append("<ol class='carousel-indicators'></ol>");
+  var indicators = $(".carousel-indicators");
+  bootCarousel.find(".carousel-inner").children(".item").each(function(index) {
+      (index === 0) ?
+      indicators.append("<li data-target='#MyCarousel' data-slide-to='" + index + "' class='active'></li>") :
+      indicators.append("<li data-target='#MyCarousel' data-slide-to='" + index + "'></li>");
+  });
 });
 
 (function () {})();
